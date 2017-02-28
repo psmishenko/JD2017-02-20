48af1df6471e5ad8714c4d4a9204174fec6ccc1b
Alex <akhmelev@gmail.com>
2017-02-28 22:51:02 +0300

    jd01_05 Init

---
+++ src/by/it/akhmelev/jd01_04/Demo.java
@@ -0,0 +1,10 @@
+package by.it.akhmelev.jd01_04;
+
+import java.io.IOException;
+
+public class Demo  {
+    //разместите так же как тут ваши варианты решения задания jd01_04 (массивы, матрицы)
+    public static void main(String[] args) throws IOException {
+        System.out.print("demo_no_git...");
+    }
+}
