485ea762b7e2d61c69f4a5a5b27ab2520de439f3
Prigozhanov <vprigozhanov@gmail.com>
2017-03-01 18:51:48 +0300

    Инверсия матрицы (done)

--- src/by/it/prigozhanov/jd01_05/math/Task1.java
+++ src/by/it/prigozhanov/jd01_05/math/Task1.java
@@ -8,7 +8,7 @@ import static java.lang.Math.*;
 public class Task1 {
     static double OptionA(double a, double b) {
         double y;
-        y = pow(cos(PI / 4), 4) + pow(sqrt(a + 1.5), 1 / 5) + pow((a * b), 8) - b / (log(abs(a * a)));
+        y = pow(cos(PI / 4), 4) + pow(a + 1.5, 1 / 5) + pow((a * b), 8) - b / (log(abs(a * a)));
         return y;
     }
 }
