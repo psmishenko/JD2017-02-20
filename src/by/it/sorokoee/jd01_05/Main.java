f824c8b2441ca16ea849901cb0214a27a8a35c32
sorokoee <sorokoee@gmail.com>
2017-03-01 12:12:33 +0300

    выполнено jd01_05

---
+++ src/by/it/sorokoee/jd01_05/Main.java
@@ -0,0 +1,9 @@
+package by.it.sorokoee.jd01_05;
+
+public class Main {
+    public static void main(String[] args) {
+//Задача1
+        System.out.println(Task1.optA(1.21,0.371));
+        System.out.println(Task1.optB(1.21,0.371));
+    }
+}
