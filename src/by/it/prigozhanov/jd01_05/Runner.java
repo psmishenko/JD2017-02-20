a11b3ebd5e3469e7ad51f1aad21d0e447bde1c65
Prigozhanov <vprigozhanov@gmail.com>
2017-03-01 12:22:45 +0300

    Test

---
+++ src/by/it/prigozhanov/jd01_05/Runner.java
@@ -0,0 +1,10 @@
+package by.it.prigozhanov.jd01_05;
+
+/**
+ * Created by v-omf on 3/1/2017.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(TaskA.Task1(-3.45, 349.1));
+    }
+}
