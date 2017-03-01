1d91aa663a36e2960d5e494a59d50f37c7fef33d
RadivonikAlexander <olsy@tut.by>
2017-03-01 12:10:36 +0300

    Задание 1

---
+++ src/by/it/radivonik/jd01_05/Runner.java
@@ -0,0 +1,12 @@
+package by.it.radivonik.jd01_05;
+
+/**
+ * Created by Radivonik on 01.03.2017.
+ */
+public class Runner {
+    public static void main (String[] args) {
+        System.out.println(Task1.calc1(0.3,-21.17));
+        System.out.println(Task1.calc2(0.3,-21.17));
+
+    }
+}
