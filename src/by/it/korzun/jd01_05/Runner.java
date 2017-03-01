62f0684ca4d9e98e2afe4218a4917611d350f19c
GitTooth <stelth47@gmail.com>
2017-03-01 12:19:08 +0300

    Начал выполнение jd01_05

---
+++ src/by/it/korzun/jd01_05/Runner.java
@@ -0,0 +1,11 @@
+package by.it.korzun.jd01_05;
+
+public class Runner {
+    public static void main(String[] args){
+        System.out.println("Вариант №12");
+        System.out.println(Task1.run());
+        System.out.println(Task2.run(3.24, 5.8));
+        Task3.run();
+
+    }
+}
