3fa8468177219b2fe3cddcfe3e14436ede3cc90e
sorokoee <sorokoee@gmail.com>
2017-03-01 12:17:07 +0300

    выполнено jd01_05

---
+++ src/by/it/sorokoee/jd01_02/Main.java
@@ -0,0 +1,33 @@
+package by.it.sorokoee.jd01_02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        String line = Util.consoleReadLine();
+        int[] a = Util.lineToIntArray(line);
+        Util.lineToIntArray(line);
+        TaskA.getMaxMin(line, a);
+        TaskA.lenMean(line, a);
+        for (int i : a) {
+            if (TaskA.uniqueDigits(i)) {
+                System.out.println("Найдено число из разных цифр: " + i);
+
+                break;
+            }
+
+             }
+
+        TaskB.numberMonth();
+        TaskB.intervalAcces();
+        TaskB.matrix();
+        TaskC.getPrintMatrix();
+
+    }
+
+
+}
+
+
+
