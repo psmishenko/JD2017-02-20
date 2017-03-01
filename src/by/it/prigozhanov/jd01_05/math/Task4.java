cff15c7f495a9b3bc57337f82964adf6795813a6
Prigozhanov <vprigozhanov@gmail.com>
2017-03-01 20:37:21 +0300

    Инверсия матрицы (done) Задание 5

---
+++ src/by/it/prigozhanov/jd01_05/math/Task4.java
@@ -0,0 +1,19 @@
+package by.it.prigozhanov.jd01_05.math;
+import static java.lang.Math.*;
+
+/**
+ * Created by v-omf on 3/1/2017.
+ */
+public class Task4 {
+    static void calculate() {
+        double m = 1;
+        for (int d = 23; d <= 39; d++) {
+            for (double t = 2; t <= 3 ; t+=0.1) {
+                m*=log((abs(d)));
+            }
+        }
+        System.out.println(m);
+    }
+
+
+}
