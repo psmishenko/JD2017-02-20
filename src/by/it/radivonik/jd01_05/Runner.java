737a7fda59c2ba0dce808fe6996c5ba58138b51d
RadivonikAlexander <olsy@tut.by>
2017-03-02 22:15:39 +0300

    Урок 05 Задание 1

--- src/by/it/radivonik/jd01_05/Runner.java
+++ src/by/it/radivonik/jd01_05/Runner.java
@@ -5,8 +5,21 @@ package by.it.radivonik.jd01_05;
  */
 public class Runner {
     public static void main (String[] args) {
+        // Вариант 8
+        System.out.println("\nВариант 8");
+
+        // Задание 1
+        System.out.println("\nЗадание 1");
         System.out.println(Task1.calc1(0.3,-21.17));
         System.out.println(Task1.calc2(0.3,-21.17));
 
+        // Задание 2
+        System.out.println("\nЗадание 2");
+
+        // Задание 3
+        System.out.println("\nЗадание 3");
+
+        // Задание 4
+        System.out.println("\nЗадание 4");
     }
 }
