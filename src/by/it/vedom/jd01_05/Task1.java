39502f10105edcd992127b68784ae03c024c79d3
VladVedom <house9494@mail.ru>
2017-03-02 16:30:03 +0300

    Решено 2, 3, 4, 5, 6 задания jd01_05

--- src/by/it/vedom/jd01_05/Task1.java
+++ src/by/it/vedom/jd01_05/Task1.java
@@ -1,7 +1,7 @@
 package by.it.vedom.jd01_05;
 
 import static java.lang.Math.*;
-import static java.lang.Math.asin;
+import static java.lang.Math.atan;
 import static java.lang.Math.pow;
 
 /**
@@ -9,14 +9,19 @@ import static java.lang.Math.pow;
  */
 public class Task1 {
 
-   public static double option1 (double x) {
+   public static double optionA (double x) {
        double a = pow(sin(PI/2+1), 2);
        double b = x*pow(3*(pow(x, 2)), 1/4);
        double c = pow(tan((pow(x,3)-1)), 3); // tg^3*(x^3-1)
-       double d = (1/tan(x/2)) - log10(17.56);
+       double d = (1/atan(x/2)) - log10(17.56);
 
        x = (a + b -c) / d;
 
+       return x;
+   }
+
+   public static double optionB (double x) {
+       x = (pow(sin(PI/2+1), 2) + x*pow(3*(pow(x, 2)), 1/4) - pow(tan((pow(x,3)-1)), 3)) / ((1/atan(x/2)) - log10(17.56));
        return x;
    }
 
