2d292f33341788e30517890edf221a8433b8e366
zzz <zz88@tut.by>
2017-03-03 12:42:22 +0300

    Class task #6

--- src/by/it/kolenda/jd01_06/TaskA.java
+++ src/by/it/kolenda/jd01_06/TaskA.java
@@ -1,8 +1,25 @@
 package by.it.kolenda.jd01_06;
 
+import java.util.regex.Pattern;
+
+import java.util.regex.Matcher;
 
 public class TaskA {
+
     public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder(Data.lukomor);
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{4,}");
+        Matcher matcher = pattern.matcher(sb);
+          while (matcher.find())     {
+              int pos = matcher.start() + 3;
+              sb.setCharAt(pos, '#');
+              if (matcher.group().length() > 6) {
+                  pos = matcher.start() + 6;
+                  sb.setCharAt(pos, '#');
+              }
+          }
+
         String[] words = Data.lukomor.split("[^а-яА-ЯёЁ]+");
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
