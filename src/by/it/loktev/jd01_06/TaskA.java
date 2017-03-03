75cdca862458c63a83c9bff9baeb5f9fad3c60ff
loktevalexey <loktev.alex.74@gmail.com>
2017-03-03 11:41:55 +0300

    занятие 6, сделано задание A

--- src/by/it/loktev/jd01_06/TaskA.java
+++ src/by/it/loktev/jd01_06/TaskA.java
@@ -1,5 +1,6 @@
 package by.it.loktev.jd01_06;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,6 +21,29 @@ public class TaskA {
                 //sb.replace(matcher.start()+6,matcher.start()+7,"#");
         }
         System.out.println(sb);
+
+        String [] word=Data.lukomor.split("[^а-яА-ЯёЁ]+");
+        System.out.println(Arrays.toString(word));
+
+        while ( word.length>0 ) {
+            String[] word2 = new String[word.length];
+            int word2len = 0;
+            String currWord=word[0];
+            int wordCount=1;
+            for (int i2=1; i2<word.length; i2++) {
+                if (currWord.equalsIgnoreCase(word[i2]))
+                    wordCount++;
+                else
+                    word2[word2len++] = word[i2];
+            }
+            System.out.println("слово "+currWord+" встречается "+wordCount);
+            //System.out.println(Arrays.toString(word2));
+            //break;
+            word=new String[word2len];
+            for (int i = 0; i <word2len ; i++) {
+                word[i]=word2[i];
+            }
+        }
 
 
     }
---
+++ src/by/it/loktev/jd01_06/TaskB.java
@@ -0,0 +1,4 @@
+package by.it.loktev.jd01_06;
+
+public class TaskB {
+}
