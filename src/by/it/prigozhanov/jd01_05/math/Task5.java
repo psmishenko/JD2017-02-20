a635d978809342966393ffa6a21b19294adaa6b7
Prigozhanov <vprigozhanov@gmail.com>
2017-03-03 20:06:45 +0300

    Math. A,B

--- src/by/it/prigozhanov/jd01_05/math/Task5.java
+++ src/by/it/prigozhanov/jd01_05/math/Task5.java
@@ -9,27 +9,22 @@ public class Task5 {
     static void calculate() {
         double l;
         double y;
-        System.out.println("Первое условие");
-        for (double x = -1.8; x <= -1; x += 0.2) {
-            l = asin(x * x);
-            y = l / 3 + sqrt(l);
-            System.out.printf("x=[%2.1f] l=[%3.2f]  y=[%2.3f]", x, l, y);
-            System.out.println();
+        for (double x = -3; x < 0.3; x += 0.2) {
+            if (x > -2 && x <= -1) {
+                l = asin(x * x);
+                y = l / 3 + sqrt(l);
+                System.out.printf("x=[%1.2f] y=[%2.2f]\n",x, y);
+            } if (x > -1 && x < 0.2) {
+                l = acos(x * x);
+                y = l / 3 + sqrt(l);
+                System.out.printf("x=[%1.2f] y=[%2.2f]\n",x, y);
+            }
+            if (x >= 0.2) //remember this question!!
+            {
+                l = atan(x * x);
+                y = l / 3 + sqrt(l);
+                System.out.printf("x=[%1.2f] y=[%2.2f]\n",x, y);
+            }
         }
-        System.out.print("Второе условие");
-        System.out.println("");
-        for (double x = -1; x <= 0.2; x += 0.2) {
-            l = acos(x * x);
-            y = l / 3 + sqrt(l);
-            System.out.printf("x=[ %-4.1f ] l=[%3.2f]  y=[%2.3f]", x, l, y);
-            System.out.println();
-        }
-
-        System.out.println("Третье условие");
-        double x = 0.2;
-        l = atan(x * x);
-        y = l / 3 + sqrt(l);
-        System.out.printf("x=[ %2.1f ] l=[%3.2f]  y=[%2.3f]", x, l, y);
-        System.out.println();
     }
 }
