f1c030438cf71245b23b5a32fb8d20b0ebb8bca6
RadivonikAlexander <olsy@tut.by>
2017-03-03 09:59:47 +0300

    Урок 05 Задание 1-4

--- src/by/it/radivonik/jd01_05/Task4.java
+++ src/by/it/radivonik/jd01_05/Task4.java
@@ -1,7 +1,27 @@
 package by.it.radivonik.jd01_05;
 
+import static java.lang.Math.*;
+
 /**
  * Created by Radivonik on 02.03.2017.
  */
 public class Task4 {
+    public static void calc(double a, double b1, double b2, double db) {
+        double z;
+        double s;
+        double b = b1;
+
+        System.out.printf("%10s  %10s", "b", "z");
+        while (b <= b2) {
+            z = 1.38 - a;
+            s = 0;
+            for (int c = 1; c <= 12; c++) {
+                s = s + pow(c + b,2);
+            }
+            z = z + s;
+            System.out.printf("\n%10.2f  %10.5f", b, z);
+            b = b + db;
+        }
+        System.out.println();
+    }
 }
