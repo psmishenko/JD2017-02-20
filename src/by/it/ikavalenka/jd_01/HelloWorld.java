0b468908d804c3b837da5e003b5b6ff4c66285e4
ikavalenka <z.yellow@yandex.by>
2017-03-05 05:13:56 +0300

    Tasks for jd_01/02/05

---
+++ src/by/it/ikavalenka/jd_01/HelloWorld.java
@@ -0,0 +1,12 @@
+package by.it.ikavalenka.jd_01;
+
+/**
+ * Created by USER on 22.02.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
---
+++ src/by/it/ikavalenka/jd_01/hwsetter.java
@@ -0,0 +1,11 @@
+package by.it.ikavalenka.jd_01;
+
+/**
+ * Created by USER on 22.02.2017.
+ */
+public class hwsetter {
+    public void setHW(String HelloWORLD33333){
+    System.out.print("HelloWORLD33333");
+
+    }
+}
