32e9749a9a125696560732e5e314a6bd41082108
loktevalexey <loktev.alex.74@gmail.com>
2017-03-05 13:31:23 +0300

    jd01_07

--- src/by/it/loktev/jd01_06/TaskB.java
+++ src/by/it/loktev/jd01_06/TaskB.java
@@ -1,5 +1,6 @@
 package by.it.loktev.jd01_06;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,7 +24,25 @@ public class TaskB {
                 glasGlasCount++;
             }
         }
-        System.out.println("Слов, начинающихся и заканчивающихся на гласную букву: "+glasGlasCount);
+        System.out.println("Слов, начинающихся и заканчивающихся на гласную букву: "+glasGlasCount+"\n\n");
+
+        // получим предложения
+        Pattern phrasesSplitPattern = Pattern.compile("[\\.!?]+ *");
+        String [] phrases=phrasesSplitPattern.split(Data.lukomor);
+        String [] sortPhrases=new String[phrases.length];
+        for ( int i=0; i<phrases.length; i++ ) {
+            String phrase=phrases[i].replace("\n"," ").trim(); // уберём переносы строк и лишние пробелы на концах
+            String [] phraseWords=phrase.split("[^а-яА-ЯёЁ]+");
+            System.out.println("Слов: "+phraseWords.length+", предложение:");
+            System.out.println(phrase);
+            sortPhrases[i]=String.format("%05d",phraseWords.length)+phrase; // дописываем слева к предложению кол-во слов в нём фиксированной длины - для простоты сортировки, раз уж мы не умеем ничего другого
+        }
+
+        Arrays.sort(sortPhrases);
+
+        System.out.println("\nПредложения, отсортированные по количеству слов:");
+        for ( String phrase : sortPhrases )
+            System.out.println(phrase.substring(5));
 
 
     }
