2130e74a174e1dd7fbd09a8fef2730d414003cdf
RadivonikAlexander <olsy@tut.by>
2017-03-07 01:15:31 +0300

    Занятие 06 Задания 01-06

--- src/by/it/radivonik/jd01_06/TaskC.java
+++ src/by/it/radivonik/jd01_06/TaskC.java
@@ -39,6 +39,85 @@ public class TaskC {
         System.out.println();
     }
 
+    // Формирование строки из миллиона символов
     public static void zad6() {
+        String[] aWord = Data.lukomor.split("[^а-яА-ЯёЁ]+");
+        System.out.printf("\nString:");
+        System.out.printf(
+                "\nВремя формирования: %d миллисекунд",
+                strMillionString(aWord));
+        System.out.printf("\nStringBuilder:");
+        System.out.printf(
+                "\nВремя формирования: %d миллисекунд",
+                strMillionStringBuilder(aWord));
+    }
+
+    static int MaxCountChar = 1000000;
+
+    // Использование String
+    public static long strMillionString(String[] aWord) {
+        long start = System.currentTimeMillis();
+        String s = "";
+        int len = 0;
+
+        do {
+            String w = nextWord(aWord,MaxCountChar-len);
+            s += w;
+            len += w.length();
+        }
+        while (len < MaxCountChar);
+
+        System.out.printf("\nДлина строки: %d",len);
+        System.out.printf("\nПервые 50 символов: %s",s.substring(0,49));
+        return System.currentTimeMillis() - start;
+    }
+
+    // Использование StringBuilder
+    public static long strMillionStringBuilder(String[] aWord) {
+        long start = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder(MaxCountChar);
+        int len = 0;
+
+        do {
+            String w = nextWord(aWord,MaxCountChar-len);
+            sb.append(w);
+            len += w.length();
+        }
+        while (len < MaxCountChar);
+
+        System.out.printf("\nДлина строки: %d",len);
+        System.out.printf("\nПервые 50 символов: %s",sb.substring(0,49));
+        return System.currentTimeMillis() - start;
+    }
+
+    // Cлучайный выбор слова
+    public static String nextWord(String[] aWord, int lenMax) {
+        String res = "";
+        int index = (int) (Math.random() * aWord.length);
+
+        if (aWord[index].length() == lenMax)
+            res = aWord[index];
+        else if (aWord[index].length() < lenMax + 1)
+            res = aWord[index] + " ";
+        else { // поиск подходящего по длине слова
+            int i = 0;
+            while (i <= index) {
+                for (int j = 0; j < aWord.length; j++) {
+                    if (aWord[j].length() == lenMax) {
+                        if (i == index)
+                            res = aWord[j];
+                        i++;
+                    }
+                }
+            };
+        }
+
+        if (res.length() == 0) {
+            for (int i = 0; i < lenMax; i++) {
+                res += " ";
+            }
+        }
+
+        return res;
     }
 }
