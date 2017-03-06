a2d17212003951bdf9a3583687c0dd2e9ccce32c
Zeynalov <6361773@mail.ru>
2017-03-06 11:08:26 +0300

    jd01_05 complete

--- src/by/it/zeynalov/jd01_05/Task4.java
+++ src/by/it/zeynalov/jd01_05/Task4.java
@@ -1,17 +1,4 @@
 package by.it.zeynalov.jd01_05;
 
 public class Task4 {
-
-    public static double optionE(double a){
-        double summa = 0;
-
-        for (double b = 0.2; b <= 0.4; b += 0.02){
-            for (int c = 1; c <= 12; c++){
-                double x = c - b;
-                summa = summa + x;
-            }
-        }
-        return 1.38 * a + summa;
-    }
 }
-
