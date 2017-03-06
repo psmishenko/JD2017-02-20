7c4a822ac18c7385dcd74eca989288ec07c246f5
Zeynalov <6361773@mail.ru>
2017-03-06 11:08:26 +0300

    jd01_05 complete

--- src/by/it/zeynalov/jd01_05/Run.java
+++ src/by/it/zeynalov/jd01_05/Run.java
@@ -3,9 +3,6 @@ package by.it.zeynalov.jd01_05;
 
 import static by.it.zeynalov.jd01_05.Task1.optionA;
 import static by.it.zeynalov.jd01_05.Task1.optionB;
-import static by.it.zeynalov.jd01_05.Task2.optionC;
-import static by.it.zeynalov.jd01_05.Task3.optionD;
-import static by.it.zeynalov.jd01_05.Task4.optionE;
 import static java.lang.Math.*;
 
 public class Run {
@@ -13,17 +10,8 @@ public class Run {
     public static void main(String[] args) {
 
         //Task1
-        System.out.println("optionA: " + optionA());
-        System.out.println("optionB: " + optionB());
-
-        //Task2
-        System.out.println("optionC: " + optionC(2.0));
-
-        //Task3
-        optionD();
-
-        //Task4
-        System.out.println("optionE: " + optionE(2.408));
+        System.out.println(optionA());
+        System.out.println(optionB());
 
     }
 }
