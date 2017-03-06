4762a24851b8b823a67205f2334ceff6da50f3b0
RadivonikAlexander <olsy@tut.by>
2017-03-06 22:54:03 +0300

    Занятие 06 Задания 01-05

--- src/by/it/radivonik/jd01_06/TaskC.java
+++ src/by/it/radivonik/jd01_06/TaskC.java
@@ -6,7 +6,37 @@ import java.util.regex.*;
  * Created by Radivonik on 03.03.2017.
  */
 public class TaskC {
+    // Выравнивание текста по ширине
     public static void zad5() {
+        String[] aLine = Data.lukomor.split("[\n]");
+        int lenMax = 0;
+
+        // определение максимальной ширины строки
+        for (String s: aLine) {
+            if(lenMax < s.length())
+                lenMax = s.length();
+        }
+
+        // форматирование строк
+        for (int i = 0; i < aLine.length; i++) {
+            StringBuilder sbLine = new StringBuilder(aLine[i]);
+            int len = sbLine.length();
+
+            while (len < lenMax) { // цикл, пока длина строки не достигнет максимальной
+                Pattern pLine = Pattern.compile(" +");
+                Matcher mLine = pLine.matcher(sbLine.toString());
+                int countIns = 0; // счетчик добавленных пробелов = счетчик смещения
+                while (mLine.find()) { // поиск пробелов
+                    sbLine.insert(mLine.start()+countIns," "); // вставка пробелов
+                    len++; // счетчик длины строки
+                    countIns++; // счетчик добавленных пробелов
+                    if (len >= lenMax) // выход из цикла поиска пробелов при необходимости
+                        break;
+                }
+            }
+            System.out.printf("\n%s",sbLine);
+        }
+        System.out.println();
     }
 
     public static void zad6() {
