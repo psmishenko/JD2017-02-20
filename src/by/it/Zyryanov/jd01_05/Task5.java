d60e3595792eeda56b9430735555dc2a36afac7a
George Zyryanov <george.zyryanov@gmail.com>
2017-03-07 17:34:19 +0300

    Task 5 was changed

--- src/by/it/Zyryanov/jd01_05/Task5.java
+++ src/by/it/Zyryanov/jd01_05/Task5.java
@@ -20,21 +20,19 @@ public class Task5 {
         do {
             if (p - 1 < 0) {
                 z = p - 3.18 * cos(p + 1);
-                System.out.println("Первое условие z = " + z);
                 y = sqrt(abs((z - 2.56 * r)));
-                System.out.println("Первое условие y = " + y);
-            }
-            if (0 <= (p - 1) && (p - 1) < 3) {
+                System.out.printf("первое условие при p = [ %.1f ] y = %.2f", p, y);
+                System.out.println();
+            } else if (0 <= (p - 1) && (p - 1) < 3) {
                 z = pow(p , 3) - tan(p);
-                System.out.println("Второе условие z = " + z);
                 y = sqrt(abs((z - 2.56 * r)));
-                System.out.println("Первое условие y = " + y);
-            }
-            if (p - 1 >= 3){
+                System.out.printf("второе условие при p = [ %.1f ] y = %.2f", p, y);
+                System.out.println();
+            } else if (p - 1 >= 3){
                 z = pow((p + sin(PI/6)) ,5);
-                System.out.println("Третье условие z = " + z);
                 y = sqrt(abs((z - 2.56 * r)));
-                System.out.println("Первое условие y = " + y);
+                System.out.printf("третье условие при р = [ %.1f ] y = %.2f", p, y);
+                System.out.println();
             }
             p += 1.5;
         } while (p > -2 && p < 15);
