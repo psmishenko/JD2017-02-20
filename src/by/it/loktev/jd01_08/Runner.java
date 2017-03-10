738ba2486ad3c54ec7d06866ee567e06c64a2ca5
loktevalexey <loktev.alex.74@gmail.com>
2017-03-10 15:30:51 +0300

    занятие 8

--- src/by/it/loktev/jd01_08/Runner.java
+++ src/by/it/loktev/jd01_08/Runner.java
@@ -18,8 +18,8 @@ public class Runner {
         note1.pause();
         note1.resume();
         note1.chargeEnd();
-        note1.repairBroken();
-        note1.repairBroken(     100);
+        note1.repairBroken(); // статический полимфорфизм
+        note1.repairBroken(     100); // статический полимфорфизм
         note1.runScreenSaver("c:/program/saver.exe"); // позднее связывание - внутри Computer::runScreenSaver вызывается Notebook::run
 
         System.out.println();
@@ -30,9 +30,20 @@ public class Runner {
         phone1.pause();
         phone1.resume();
         phone1.chargeEnd();
-        phone1.repairBroken();
-        phone1.repairBroken(     500);
+        phone1.repairBroken(); // статический полимфорфизм
+        phone1.repairBroken(     500); // статический полимфорфизм
         phone1.runScreenSaver("//dev//sdcard//saver"); // позднее связывание - внутри Computer::runScreenSaver вызывается MobilePhone::run
+
+        // динамический полиморфизм
+        System.out.println();
+        Computer comp1=new MobilePhone("смартфон HTC",120, true, false);
+        System.out.println(comp1);
+        comp1.run("//dev//sdcard//digger");
+        comp1.pause();
+        comp1.resume();
+        comp1.repairBroken(); // + статический полимфорфизм
+        comp1.repairBroken(     300); // + статический полимфорфизм
+        comp1.runScreenSaver("//dev//sdcard//saver"); // + позднее связывание - внутри Computer::runScreenSaver вызывается MobilePhone::run
 
     }
 
