339edf09a79f1a2008689e27b9f52f1e06864969
ikavalenka <z.yellow@yandex.by>
2017-03-12 11:24:29 +0300

    Tasks for  reviewe: jd_08 task jd_03 absent at the moment

---
+++ src/by/it/ikavalenka/jd_08/Weapon.java
@@ -0,0 +1,14 @@
+package by.it.ikavalenka.jd_08;
+
+/**
+ * Created by USER on 10.03.2017.
+ */
+public interface Weapon {
+    void shooting();
+    void reload();
+    void aim();
+    void score();
+    void force();
+    void speed();
+    void range();
+}
