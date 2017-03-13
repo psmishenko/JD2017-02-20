a6f7003cd06f709c66b6a1017187d5b3fb07887e
Khmelov <375336849110@tut.by>
2017-03-13 12:58:35 +0300

    jd01_09 matlab

--- src/by/it/akhmelev/jd01_09/classwork_matlab/IOperation.java
+++
@@ -1,8 +0,0 @@
-package by.it.akhmelev.jd01_09.classwork_matlab;
-
-public interface IOperation {
-    Var add(Var var);
-    Var sub(Var var);
-    Var mul(Var var);
-    Var div(Var var);
-}
--- src/by/it/akhmelev/jd01_09/classwork_matlab/IVariable.java
+++
@@ -1,6 +0,0 @@
-package by.it.akhmelev.jd01_09.classwork_matlab;
-
-public interface IVariable {
-    String toString();
-    void fromString(String value);
-}
