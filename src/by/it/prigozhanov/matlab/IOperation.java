5c5c02a40f0367d04f340013cd2c42f1a49a6a5e
Prigozhanov <vprigozhanov@gmail.com>
2017-03-13 12:20:59 +0300

    JD01_08 (done)

---
+++ src/by/it/prigozhanov/matlab/IOperation.java
@@ -0,0 +1,13 @@
+package by.it.prigozhanov.matlab;
+
+/**
+ * Created by v-omf on 3/13/2017!
+ */
+public interface IOperation {
+
+    Var add(Var var);
+    Var sub(Var var);
+    Var mul(Var var);
+    Var div(Var var);
+
+}
---
+++ src/by/it/prigozhanov/matlab/Var.java
@@ -0,0 +1,31 @@
+package by.it.prigozhanov.matlab;
+
+/**
+ * Created by v-omf on 3/13/2017!
+ */
+public abstract class Var implements IOperation, IVariable {
+
+    @Override
+    public Var add(Var var) {
+        new Error("Операция сложения невозможна");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var var) {
+        new Error("Операция вычитания невозможна");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var var) {
+        new Error("Операция умножения невозможна");
+        return null;
+    }
+
+    @Override
+    public Var div(Var var) {
+        new Error("Операция деления невозможна");
+        return null;
+    }
+}
