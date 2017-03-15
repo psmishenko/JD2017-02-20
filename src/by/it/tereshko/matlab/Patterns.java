0f29ef73743416308b4bdfa9a75de77a5d4a208f
anteresh <anteresh@gmail.com>
2017-03-15 11:51:36 +0300

    classwork jd01_09 - varV has been changed for matlab

---
+++ src/by/it/tereshko/matlab/Patterns.java
@@ -0,0 +1,13 @@
+package by.it.tereshko.matlab;
+
+public interface Patterns {
+    //возможные примеры регулярных выражений (конечно лучше составить свои).
+    String exVal = "((-?)([0-9.])+)";                                   //числа
+    String exVec = "\\{((-?([0-9.])+),?)+}";                            //вектора
+    String exMat = "\\{((\\{((-?([0-9.])+),?)+}),?)+}";                 //матрицы
+    String exAny = "(" + exMat + ")|(" + exVec + ")|(" + exVal + ")";   //одно из...
+    String exOper = "[-+*/]";                                           //операция
+    String exFull = "(" + exAny + ")" +
+                    "(" + exOper + ")" +
+                    "(" + exAny + ")";                                  //выражение целиком
+}
