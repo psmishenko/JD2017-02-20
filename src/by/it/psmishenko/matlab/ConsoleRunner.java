5615fa3af2e914ae402f9c5e2874b0f93cc9844d
Paul <psmishenko@gmail.com>
2017-03-18 11:35:45 +0300

    matlab (18.03 v1)

--- src/by/it/psmishenko/matlab/ConsoleRunner.java
+++ src/by/it/psmishenko/matlab/ConsoleRunner.java
@@ -1,13 +1,26 @@
 package by.it.psmishenko.matlab;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class ConsoleRunner {
     private static void oneRes(Var var){
         if (var != null) {
             System.out.println(var);
         }
     }
-    public static void main(String[] args) {
+    static String getOneLine() throws IOException {
+        BufferedReader stBufIn = new BufferedReader(new InputStreamReader(System.in));
+        String line = stBufIn.readLine();
+        // stBufIn.close();
+        return line;
+    }
+
+    public static void main(String[] args) throws IOException {
+        boolean zxc = true;
         // тестовые строки из jd01_11
       String levelA = "2.3+3";
       String levelB = "{1,2,3}/5";
@@ -15,24 +28,35 @@ public class ConsoleRunner {
       System.out.print("A: "+levelA+" = "); oneRes(Parser.calc(levelA));
         System.out.print("B: "+levelB+" = "); oneRes(Parser.calc(levelB));
         System.out.print("C: "+levelC+" = ");  oneRes(Parser.calc(levelC));
-
-       // oneRes(Parser.calc("Ala=3"));
-        //oneRes(Parser.calc("Bor={1,2,3,4}"));
-       // oneRes(Parser.calc("Cle=5"));
-       // oneRes(Parser.calc("Ona=7"));
-       // System.out.println(Var.vars);
-
-
-
         // тестовые строки из jd01_09 для матриц
-       // oneRes(Parser.calc("{{1,2},{8,3}}+5"));
-       // oneRes(Parser.calc("{{1,2},{8,3}}-5"));
-       // oneRes(Parser.calc("{{1,2},{8,3}}*5"));
-       // oneRes(Parser.calc("{{1,2},{8,3}}/5"));
-       // oneRes(Parser.calc("{{1,2},{8,3}}*{1,2}"));
-       // oneRes(Parser.calc("{{1,2},{8,3}}*{{1,2},{8,3}}"));
-       // oneRes(Parser.calc("{{1,2},{8,3}}+{{1,2},{8,3}}"));
-       // oneRes(Parser.calc("{{1,2},{8,3}}-{{1,2},{8,3}}"));
+        // oneRes(Parser.calc("{{1,2},{8,3}}+5"));
+        // oneRes(Parser.calc("{{1,2},{8,3}}-5"));
+        // oneRes(Parser.calc("{{1,2},{8,3}}*5"));
+        // oneRes(Parser.calc("{{1,2},{8,3}}/5"));
+        // oneRes(Parser.calc("{{1,2},{8,3}}*{1,2}"));
+        // oneRes(Parser.calc("{{1,2},{8,3}}*{{1,2},{8,3}}"));
+        // oneRes(Parser.calc("{{1,2},{8,3}}+{{1,2},{8,3}}"));
+        // oneRes(Parser.calc("{{1,2},{8,3}}-{{1,2},{8,3}}"));
+        Parser.calc("Alla=3");
+        Parser.calc("Boris={1,2,3,4}");
+        Parser.calc("Cleopatra=5");
+        Parser.calc("Oleg=7");
+        do {
+            System.out.println("Введите printvar или sortvar (или иной символ для завершения)");
+            String choice = getOneLine();
+            switch (choice){
+                case "printvar":
+                    System.out.println(Var.vars);
+                    break;
+                case "sortvar":
+                    HashMap<String,Var> newVars = new HashMap<>();
+                    newVars.putAll(Var.vars);
 
+
+                    System.out.println(newVars);
+                    break;
+                    default: zxc = false;
+            }
+        }while (zxc);
     }
 }
