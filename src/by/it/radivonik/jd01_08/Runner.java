b5fed05a1cbe387c8002362d85fba9e6cc68be81
RadivonikAlexander <olsy@tut.by>
2017-03-19 15:40:06 +0300

    Калькулятор

--- src/by/it/radivonik/jd01_08/Runner.java
+++ src/by/it/radivonik/jd01_08/Runner.java
@@ -5,17 +5,25 @@ package by.it.radivonik.jd01_08;
  */
 public class Runner {
     public static void main(String[] args) {
-        Player v = new VideoPlayer("Cometa");
-        ITech iv = v;
+        Shkaf s = new BookShkaf("Книжный шкаф в зале");
 
-        iv.play();
-        iv.showState();
-        //v.showState("Тест");
-        iv.pause();
-        iv.showState();
-        iv.stop();
-        iv.showState();
-
-        System.out.println(v);
+        System.out.println("Задание к занятию JD01_08");
+        System.out.println("Распаковка");
+        s.raspakovka();
+        s.showState();
+        System.out.println("Сборка");
+        s.sborka();
+        s.showState();
+        System.out.println("Открытие");
+        s.otkrytie();
+        s.showState();
+        System.out.println("Расстановка книг");
+        ((BookShkaf)s).bookFill();
+        s.showState();
+        System.out.println("Закрытие");
+        s.zakrytie();
+        s.showState();
+        System.out.println("Перевозка");
+        s.perevozka();
     }
 }
