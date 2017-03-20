fdec357b24d7fb6bd3bc4238b3582e98f5b59a87
user <psmishenko@gmail>
2017-03-20 12:03:37 +0300

    classwork (20.03)

---
+++ src/by/it/psmishenko/jd01_12/MyCollect.java
@@ -0,0 +1,30 @@
+package by.it.psmishenko.jd01_12;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by user on 20.03.2017.
+ */
+public class MyCollect {
+    static Set<Integer> getUnion(Set<Integer> a, Set<Integer> b){
+        Set<Integer> res = new HashSet<>(b);
+        res.addAll(a);
+        // второй способ
+//        for (Integer value:a) {
+//            if(!b.contains(value))  res.add(value);
+//        }
+        return res;
+    }
+    static Set<Integer> getCross(Set<Integer> a, Set<Integer> b){
+        Set<Integer> res = new HashSet<>();
+        // первый способ
+        res.addAll(a);
+        res.retainAll(b);
+        // второй способ
+//        for (Integer value:a) {
+//         if(b.contains(value))  res.add(value);
+//        }
+        return res;
+    }
+}
