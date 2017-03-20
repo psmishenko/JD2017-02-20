e072ce6d408a6e1fe0096db8b5169f18b1d4cc43
Prigozhanov <vprigozhanov@gmail.com>
2017-03-20 11:53:28 +0300

    VarV

---
+++ src/by/it/prigozhanov/jd01_12/Runner.java
@@ -0,0 +1,13 @@
+package by.it.prigozhanov.jd01_12;
+
+/**
+ * Created by v-omf on 3/20/2017!
+ */
+public class Runner {
+    public static void main(String[] args) {
+        TaskA taskA = new TaskA();
+        taskA.runA1();
+        taskA.runA2();
+        taskA.runA3();
+    }
+}
