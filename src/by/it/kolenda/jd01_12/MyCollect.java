e0b3d105f1e5dde31b447e114e638b48fa1f2f04
zzz <zz88@tut.by>
2017-03-20 12:48:46 +0300

    Class task 01_12 20/03/2017

--- src/by/it/kolenda/jd01_12/MyCollect.java
+++ src/by/it/kolenda/jd01_12/MyCollect.java
@@ -3,14 +3,22 @@ package by.it.kolenda.jd01_12;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.List;
 
 public class MyCollect {
+
+    static Set<Integer> getUnion(Set<Integer> a, Set<Integer> b) {
+        Set<Integer> res = new HashSet<>(b);
+        res.addAll(a);
+
+        return res;
+    }
 
     static Set<Integer> getCross(Set<Integer> a, Set<Integer> b) {
         Set<Integer> res = new HashSet<>(b);
         //first
         res.addAll(a);
-
+        res.retainAll(b);
 
 //        second
 //        for (Integer value: a){
@@ -20,9 +28,6 @@ public class MyCollect {
         return res;
     }
 
-    static Set<Integer> getUnion(Set<Integer> a, Set<Integer> b) {
-        Set<Integer> res = new HashSet<>(b);
-        res.addAll(a);
-        return res;
+
     }
-}
+
--- src/by/it/kolenda/jd01_12/Runner.java
+++ src/by/it/kolenda/jd01_12/Runner.java
@@ -6,7 +6,9 @@ public class Runner {
     public static void main(String[] args) {
 
         TaskA taskA = new TaskA();
-
+        taskA.runA1();
+        taskA.runA2();
+        taskA.runA3();
 
     }
 }
