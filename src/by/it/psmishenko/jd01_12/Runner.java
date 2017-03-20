d61b542fa469d9bf7d2602c7516e42549d09709b
user <psmishenko@gmail>
2017-03-20 12:03:37 +0300

    classwork (20.03)

---
+++ src/by/it/psmishenko/jd01_12/Runner.java
@@ -0,0 +1,16 @@
+package by.it.psmishenko.jd01_12;
+
+/**
+ * Created by user on 20.03.2017.
+ */
+public class Runner {
+    public static void main(String[] args) {
+    TaskA taskA = new TaskA();
+     //   taskA.runA1();
+        //
+        //taskA.runA2();
+        taskA.runA3();
+        // b1 - hashmap
+        // b2 - nanotime
+    }
+}
