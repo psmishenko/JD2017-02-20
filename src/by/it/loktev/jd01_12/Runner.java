9db8f137d5fbfd12d263b9820065dd9e3bb7ba24
loktevalexey <loktev.alex.74@gmail.com>
2017-03-21 13:46:54 +0300

    jd01_12

--- src/by/it/loktev/jd01_12/Runner.java
+++ src/by/it/loktev/jd01_12/Runner.java
@@ -4,6 +4,7 @@ public class Runner {
 
     public static void main(String[] args) {
 
+        /*
         TaskA taskA=new TaskA();
         System.out.println("=== A1");
         taskA.runA1();
@@ -11,6 +12,11 @@ public class Runner {
         taskA.runA2();
         System.out.println("=== A3");
         taskA.runA3();
+        */
+
+        TaskB taskB=new TaskB();
+        System.out.println("=== B1");
+        taskB.runB1();
 
     }
 
