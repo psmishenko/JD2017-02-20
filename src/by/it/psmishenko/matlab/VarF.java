c9ae09f9861c69c8aa0f817a988edfcae22a5d73
user <psmishenko@gmail>
2017-03-22 12:23:38 +0300

    classwork (22.03)

--- src/by/it/psmishenko/matlab/VarF.java
+++ src/by/it/psmishenko/matlab/VarF.java
@@ -5,7 +5,7 @@ public class VarF extends Var {
     public Double value;
 
     @Override
-    public Var add(Var var) {
+    public Var add(Var var) throws MathException {
         if (var instanceof VarF){
             VarF operand = (VarF) var;
             return  new VarF(this.value+operand.value);
@@ -14,7 +14,7 @@ public class VarF extends Var {
 
 
     @Override
-    public Var sub(Var var) {
+    public Var sub(Var var) throws MathException {
         if (var instanceof VarF){
             VarF operand = (VarF) var;
             return  new VarF(this.value - operand.value);
@@ -22,7 +22,7 @@ public class VarF extends Var {
     }
 
     @Override
-    public Var mul(Var var) {
+    public Var mul(Var var) throws MathException {
         if (var instanceof VarF){
             VarF operand = (VarF) var;
             return  new VarF(this.value * operand.value);
@@ -30,9 +30,10 @@ public class VarF extends Var {
     }
 
     @Override
-    public Var div(Var var) {
+    public Var div(Var var) throws MathException {
         if (var instanceof VarF){
             VarF operand = (VarF) var;
+            if(operand.value==0) throw new MathException("Деление на ноль");
             return  new VarF(this.value / operand.value);
         } else return super.div(var);
     }
