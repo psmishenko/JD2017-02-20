6c1e039f2b29f90fbb95ddd243238698a59c0b3b
Paul <psmishenko@gmail.com>
2017-03-22 17:45:21 +0300

    jd01_12 (A+B) (22.03)

--- src/by/it/psmishenko/jd01_12/Runner.java
+++ src/by/it/psmishenko/jd01_12/Runner.java
@@ -1,16 +1,54 @@
 package by.it.psmishenko.jd01_12;
 
-/**
- * Created by user on 20.03.2017.
- */
+import java.util.Scanner;
+
+
 public class Runner {
     public static void main(String[] args) {
-    TaskA taskA = new TaskA();
-     //   taskA.runA1();
-        //
-        //taskA.runA2();
-        taskA.runA3();
-        // b1 - hashmap
-        // b2 - nanotime
+        boolean zxc = true ;
+        do {
+            System.out.println("a,b or c?");
+            Scanner scanner = new Scanner(System.in);
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "a":
+                    TaskA taskA = new TaskA();
+                    taskA.runA1(); System.out.println("-------------------------------");
+                    taskA.runA2(); System.out.println("-------------------------------");
+                    taskA.runA3(); System.out.println("-------------------------------");
+                    break;
+                case "b":
+                    TaskB taskB = new TaskB();
+                    taskB.runB1(Data.test);
+                    //taskB.runB1(Data.text);
+                    int n = 800; // N для B2
+                    Timer t = new Timer();
+                    taskB.processArray(n);
+                    System.out.println("ArrayList: " + t);
+                    taskB.processLinked(n);
+                    System.out.println("LinkedList: " + t);
+                    break;
+                case "c":
+                    break;
+                default:
+                    zxc = false;
+            }
+
+        }while (zxc);
+    }
+    public static class Timer {
+        private long iniTime;
+        private Double Delta;
+
+        public Timer() {
+            iniTime = System.nanoTime();
+        }
+
+        public String toString() {
+            Delta = (double) (System.nanoTime() - iniTime) / 1000;
+            iniTime = System.nanoTime();
+
+            return "Прошло " + Delta.toString() + " микросекунд.";
+        }
     }
 }
