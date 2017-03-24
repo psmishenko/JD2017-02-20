5d2dc6ffbdf13c16409af0a22c8f2ab55372e888
loktevalexey <loktev.alex.74@gmail.com>
2017-03-24 12:03:31 +0300

    jd01_14

--- src/by/it/loktev/jd01_14/TaskB.java
+++ src/by/it/loktev/jd01_14/TaskB.java
@@ -1,6 +1,7 @@
 package by.it.loktev.jd01_14;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -11,20 +12,25 @@ public class TaskB {
         String basePath=System.getProperty("user.dir")+"/src/by/it/loktev/jd01_14/";
         String fileName=basePath+"taskB.txt";
 
-        try (
-                FileReader fr = new FileReader(fileName);
-                BufferedReader bo = new BufferedReader(fr);
-        ) {
+        StringBuffer sb=new StringBuffer();
 
-            StringBuffer sb=new StringBuffer();
+        try (
+            FileReader fr = new FileReader(fileName);
+            BufferedReader bo = new BufferedReader(fr);
+        ) {
             while ( bo.ready() ){
                 sb.append(bo.readLine()+"\n");
             }
-            System.out.println(sb);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        String commaRE="[,.!?;-]";
+        int countComma=(" "+sb.toString()+" ").split(commaRE).length-1;
+        String wordRE="[а-яА-ЯёЁ]+";
+        int countWord=(" "+sb.toString()+" ").split(wordRE).length-1;
+        System.out.println("Знаков препинания: "+countComma);
+        System.out.println("Слов: "+countWord);
 
     }
 
---
+++ src/by/it/loktev/jd01_14/TaskC.java
@@ -0,0 +1,13 @@
+package by.it.loktev.jd01_14;
+
+public class TaskC {
+
+    String basePath=System.getProperty("user.dir")+"/src/by/it/loktev/jd01_14/";
+
+    public static void taskC() {
+
+
+
+    }
+
+}
