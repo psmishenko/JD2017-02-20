e9930f8de04c3a772c5483df262c24a4e3024074
Paul <psmishenko@gmail.com>
2017-03-25 15:51:15 +0300

    jd01_14  mod. (25.03)

--- src/by/it/psmishenko/jd01_14/TaskC.java
+++ src/by/it/psmishenko/jd01_14/TaskC.java
@@ -1,17 +1,21 @@
 package by.it.psmishenko.jd01_14;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 24.03.2017.
  */
 public class TaskC {
-    static void processFolder(File ob){
-        if(ob.isFile()) System.out.println("File: "+ob.getName());
+    static void processFolder(File ob,List<String> files,List<String> directory){
+        String delimiter = "      ";
+        if(ob.isFile()) {System.out.println("File:"+delimiter+ob.getName()); files.add(ob.getName());}
         else if(ob.isDirectory()) {
-            System.out.println("Dir: "+ob.getName());
+          System.out.println("Dir: "+ob.getName()); directory.add(ob.getName());
             File dir[] = ob.listFiles();
-            if(dir.length>0&&dir!=null) for (File f:dir) processFolder(f);
+            if(dir.length>0&&dir!=null) for (File f:dir) processFolder(f,files,directory);
+            System.out.println("---------END OF DIRECTORY----------------------------");
         }
     }
 
@@ -19,7 +23,12 @@ public class TaskC {
         String root=System.getProperty("user.dir");
         String path = root + "/src/by/it/psmishenko/jd01_14/";
         File file = new File(path);
-        processFolder(file);
-// доделать с коллекциями
+        List<String> fileList = new ArrayList<>();
+        List<String > dirList = new ArrayList<>();
+        processFolder(file, fileList,dirList);
+        System.out.println("Печать только файлов:");
+        for (String i:fileList) System.out.println("File name:   "+i);
+        System.out.println("Печать только каталогов:");
+        for (String j:dirList) System.out.println("Dir name:   "+j);
     }
 }
