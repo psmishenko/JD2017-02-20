992b89184448e6ee3ba323c2e97fac54143d8f81
Paul <psmishenko@gmail.com>
2017-03-26 14:52:13 +0300

    jd01_12 end

--- src/by/it/psmishenko/jd01_12/Runner.java
+++ src/by/it/psmishenko/jd01_12/Runner.java
@@ -15,24 +15,24 @@ public class Runner {
             switch (choice) {
                 case "a":
                     TaskA taskA = new TaskA();
-                    taskA.runA1(); System.out.println("-------------------------------");
-                    taskA.runA2(); System.out.println("-------------------------------");
-                    taskA.runA3(); System.out.println("-------------------------------");
+                    System.out.println("#1:"); taskA.runA1(); System.out.println("-------------------------------");
+                    System.out.println("#2:"); taskA.runA2(); System.out.println("-------------------------------");
+                    System.out.println("#3:"); taskA.runA3(); System.out.println("-------------------------------");
                     break;
                 case "b":
                     TaskB taskB = new TaskB();
                     //taskB.runB1(Data.test);
-                    taskB.runB1(Data.text);
+                    System.out.println("#1:"); taskB.runB1(Data.text);
                     int n = 800; // N для B2
                     Timer t = new Timer();
-                    taskB.processArray(n);
+                    System.out.println("#2:");taskB.processArray(n);
                     System.out.println("ArrayList: " + t);
                     taskB.processLinked(n);
                     System.out.println("LinkedList: " + t);
                     break;
                 case "c":
                     TaskC taskC = new TaskC();
-                    taskC.runC1();System.out.println("-------------------------------");
+                    System.out.println("#1:");taskC.runC1();System.out.println("-------------------------------");
                     Set<String> a1 = new HashSet<>(); Set<String> b1 = new HashSet<>();
                     Set<Double> a2 = new HashSet<>(); Set<Double> b2 = new HashSet<>();
                     for (int i = 0; i <3 ; i++) {
@@ -43,9 +43,9 @@ public class Runner {
                       b1.add(Data.arr2[i]);
                       b2.add(3.14-i);
                     }
+                    System.out.println("#2:");
                     taskC.runC2(a1,b1);System.out.println("-------------------------------");
                     taskC.runC2(a2,b2);System.out.println("-------------------------------");
-                    taskC.runC3("{{ahahaha}{fkfkf}[dkfmdk](ddd)");
                     break;
                 default:
                     zxc = false;
