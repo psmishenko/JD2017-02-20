20a307cd202df8483af355fb911e274cb3c8c0e0
loktevalexey <loktev.alex.74@gmail.com>
2017-03-27 11:55:44 +0300

    jd01_15

--- src/by/it/loktev/jd01_15/TaskC.java
+++ src/by/it/loktev/jd01_15/TaskC.java
@@ -16,8 +16,24 @@ public class TaskC {
         currFolder=new File(basePath);
     }
 
-    private void showDir(){
+    private void changeDir(String newDir){
 
+        try {
+            String newPath = currFolder.getCanonicalPath()+"/"+newDir;
+            File newFolder=new File(newPath);
+            if (newFolder.isDirectory()) {
+                currFolder = newFolder;
+                System.out.println("каталог сменён на " + newFolder.getCanonicalPath());
+            }
+            else
+                System.out.println("невозможно сменить каталог");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+        private void showDir(){
 
                 /*
  Содержимое папки D:\WORK\Курсы\Java\JD2017-02-20\src\by\it\loktev\jd01_15
@@ -37,7 +53,9 @@ public class TaskC {
             System.out.println("Содержимое папки "+currFolder.getCanonicalPath());
 
             int filesCount=0;
+            long totalFilesSize=0;
             int foldersCount=0;
+            long totalSpace=0;
 
             SimpleDateFormat sdf=new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
             File [] files=currFolder.listFiles();
@@ -47,20 +65,24 @@ public class TaskC {
                 fileStr+=sdf.format(f.lastModified());
                 if ( f.isDirectory() ){
                     fileStr+=" <DIR> ";
-                    fileStr+="         ";
+                    fileStr+="           ";
                     foldersCount++;
                 }
                 else {
                     fileStr+="       ";
-                    fileStr+=String.format("%9d",f.length());
+                    fileStr+=String.format("%,9d",f.length());
                     filesCount++;
+                    totalFilesSize+=f.length();
                 }
                 fileStr+=" "+f.getName();
 
+                totalSpace=f.getFreeSpace();
+
                 System.out.println(fileStr);
             }
-            System.out.println(filesCount+" файлов");
-            System.out.println(foldersCount+" папок");
+            System.out.println(String.format("%,6d",filesCount)+" файлов, "+String.format("%,9d",totalFilesSize)+" байт");
+            System.out.println(String.format("%,6d",foldersCount)+" папок");
+            System.out.println(String.format("%,9d",totalSpace)+" байт свободно");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -72,15 +94,19 @@ public class TaskC {
 
     public void listenCommands(){
 
+
         Scanner scanner=new Scanner(System.in);
 
         while ( true ){
+            System.out.print("> ");
             String command = scanner.nextLine();
 
             if ( command.equals("quit") )
                 break;
             else if ( command.equals("dir") )
                 showDir();
+            else if ( command.startsWith("cd ") )
+                changeDir(command.substring(3));
             else
               System.out.println("Unknown command");
         }
