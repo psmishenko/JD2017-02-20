accf2642a0b4d06cb8b76801028cd3109ef7c2d8
GitTooth <stelth47@gmail.com>
2017-03-27 19:05:59 +0300

    Добавлен: jd01_15

---
+++ src/by/it/korzun/jd01_15/Runner.java
@@ -0,0 +1,12 @@
+package by.it.korzun.jd01_15;
+
+public class Runner {
+    public static void main(String[] args) {
+        TaskA taskA = new TaskA();
+        taskA.run();
+        TaskB taskB = new TaskB();
+        taskB.run();
+        TaskC taskC = new TaskC();
+        taskC.run();
+    }
+}
