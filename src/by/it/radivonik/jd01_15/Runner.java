fbd1e9f7c817dc03511004e5faed79f1bbe298ab
RadivonikAlexander <olsy@tut.by>
2017-03-27 17:14:03 +0300

    jd01_15

---
+++ src/by/it/radivonik/jd01_15/Runner.java
@@ -0,0 +1,11 @@
+package by.it.radivonik.jd01_15;
+
+/**
+ * Created by aleksandr.radivonik on 27.03.2017.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        
+    }
+}
+
