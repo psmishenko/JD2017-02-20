088ebc83803f954dd4ca62cebf60db4dea420d8a
Alex <akhmelev@gmail.com>
2017-03-28 23:14:27 +0300

    jd01_15 Matlab Demo

---
+++ src/by/it/akhmelev/jd01_15_matlab_demo/MathExcepton.java
@@ -0,0 +1,7 @@
+package by.it.akhmelev.jd01_15_matlab_demo;
+
+public class MathExcepton extends Exception {
+    public MathExcepton(String message) {
+        super(message);
+    }
+}
---
+++ src/by/it/akhmelev/jd01_15_matlab_demo/VarM.java
@@ -0,0 +1,15 @@
+package by.it.akhmelev.jd01_15_matlab_demo;
+
+/**
+ * @author i.sukach
+ */
+public class VarM extends Var {
+    @Override
+    public void fromString(String value) {
+
+    }
+
+    public VarM(String value) {
+        fromString(value);
+    }
+}
