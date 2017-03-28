de52311b25b7366dfaacc402adbeec4a10d54a44
sorokoee <sorokoee@gmail.com>
2017-03-28 16:40:18 +0300

    выполнено jd01_15( A ,  часть B без удаления комментариев) и calculator (методы записи и получения Map реализованы в ConsolRunner)

--- src/by/it/sorokoee/jd01_02/Main.java
+++ src/by/it/sorokoee/jd01_02/Main.java
@@ -5,23 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String line = Util.consoleReadLine();
-        int[] a = Util.lineToIntArray(line);
-        Util.lineToIntArray(line);
-        TaskA.getMaxMin(line, a);
-        TaskA.lenMean(line, a);
-        for (int i : a) {
-            if (TaskA.uniqueDigits(i)) {
-                System.out.println("Найдено число из разных цифр: " + i);
-
-                break;
-            }
-
-             }
-
-        TaskB.numberMonth();
-        TaskB.intervalAcces();
-        TaskB.matrix();
+//        String line = Util.consoleReadLine();
+//        int[] a = Util.lineToIntArray(line);
+//        Util.lineToIntArray(line);
+//        TaskA.getMaxMin(line, a);
+//        TaskA.lenMean(line, a);
+//        for (int i : a) {
+//            if (TaskA.uniqueDigits(i)) {
+//                System.out.println("Найдено число из разных цифр: " + i);
+//
+//                break;
+//            }
+//
+//             }
+//
+//        TaskB.numberMonth();
+//        TaskB.intervalAcces();
+//        TaskB.matrix();
         TaskC.getPrintMatrix();
 
     }
