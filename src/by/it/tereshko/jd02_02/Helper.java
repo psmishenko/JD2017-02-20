442572bfe8073f2a6b8f4c37eaab3bf3f0737209
anteresh <anteresh@gmail..com>
2017-03-31 14:51:58 +0200

    jd02_02 lesson added

---
+++ src/by/it/tereshko/jd02_02/Helper.java
@@ -0,0 +1,33 @@
+package by.it.tereshko.jd02_02;
+
+import java.util.Random;
+
+public class Helper {
+
+    public Helper() {
+    }
+
+    private static Random random;
+
+    static {
+        long time = System.currentTimeMillis();
+        random = new Random(time);
+    }
+
+    protected static int getRandom(int max) {
+        return random.nextInt(max);
+    }
+
+    protected static int getRandom(int from, int to) {
+        return from + random.nextInt(to - from + 1);
+    }
+
+    protected static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
