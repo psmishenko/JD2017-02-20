352e56562effdeb22ca3af13067fbb5f24ee8452
GitTooth <stelth47@gmail.com>
2017-04-03 11:55:30 +0300

    Добавлен jd02_02

---
+++ src/by/it/korzun/jd02_02/IBuyer.java
@@ -0,0 +1,11 @@
+package by.it.korzun.jd02_02;
+
+public interface IBuyer {
+    void enterToMarket(); //вошел в магазин (мгновенно)
+
+    void chooseGoods(); //выбрал товар (от 0,5 до 2 секунд)
+
+    void gotoQueue();
+
+    void goToOut();
+}
