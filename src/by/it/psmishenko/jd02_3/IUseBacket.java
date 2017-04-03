86b2d4d8ded3606c35ced7f447b8cc9aaa125261
user <psmishenko@gmail>
2017-04-03 12:19:30 +0300

    cw 03.04

---
+++ src/by/it/psmishenko/jd02_3/IUseBacket.java
@@ -0,0 +1,10 @@
+package by.it.psmishenko.jd02_3;
+
+/**
+ * Created by user on 29.03.2017.
+ */
+public interface IUseBacket {
+    void takeBacket();
+    void putGoodsToBacket(int i, Good good);
+    void backBacket();
+}
