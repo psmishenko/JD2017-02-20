7f110bc8ac44106e68b741bc51d79d82e2961ef5
user <psmishenko@gmail>
2017-04-03 12:19:30 +0300

    cw 03.04

---
+++ src/by/it/psmishenko/jd02_3/IBuyer.java
@@ -0,0 +1,11 @@
+package by.it.psmishenko.jd02_3;
+
+/**
+ * Created by user on 29.03.2017.
+ */
+public interface IBuyer {
+    void enterToMarket();
+    void chooseGoods();
+    void gotoQueue();
+    void goToOut();
+}
