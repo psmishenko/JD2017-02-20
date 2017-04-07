147c5bc87b39818882699c04948bbbba1ca58f1b
zzz <zz88@tut.by>
2017-04-07 12:13:41 +0300

    jd02_05

---
+++ src/by/it/kolenda/jd02_05/process.bat
@@ -0,0 +1,6 @@
+FOR %%I In (*txt) DO (
+
+"D:\students\Kolenda\JD2017-02-20\src\by\it\kolenda\jd02_05>native2ascii.exe" -encoding utf-8 messages.txt messages.properties
+"D:\students\Kolenda\JD2017-02-20\src\by\it\kolenda\jd02_05>native2ascii.exe" -encoding utf-8 messages_it_IT.txt messages_it_IT.properties
+"D:\students\Kolenda\JD2017-02-20\src\by\it\kolenda\jd02_05>native2ascii.exe" -encoding utf-8 messages_ru_RU.txt messages_ru_RU.properties
+)
