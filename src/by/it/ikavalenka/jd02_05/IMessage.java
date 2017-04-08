74fc6c6ece2efd394318cf8a1c47789f06747ef8
ikavalenka <z.yellow@yandex.by>
2017-04-08 04:34:24 +0300

    TASK fo REVIEW: jd02_05

---
+++ src/by/it/ikavalenka/jd02_05/IMessage.java
@@ -0,0 +1,10 @@
+package by.it.ikavalenka.jd02_05;
+
+/**
+ * Created by USER on 07.04.2017.
+ */
+public interface IMessage {
+    String WELCOME = "Main.welcome";
+    String HOWDOYOUDO = "Main.HowDoYouDo";
+    String MYNAME = "Main.MyName";
+}
