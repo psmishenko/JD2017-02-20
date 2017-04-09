85bff4378224f9821a83f1d2cb008bec80004e4a
ikavalenka <z.yellow@yandex.by>
2017-04-10 02:22:16 +0300

    TASK fo REVIEW: jd02_03  in progress

---
+++ src/by/it/ikavalenka/jd02_03/IBuyer.java
@@ -0,0 +1,9 @@
+package by.it.ikavalenka.jd02_03;
+
+interface IBuyer {
+    void enterToMarket();
+    void chooseGoods();
+    void goToQueue() throws InterruptedException;
+    void goToOut();
+}
+
---
+++ src/by/it/ikavalenka/jd02_03/ICashier.java
@@ -0,0 +1,11 @@
+package by.it.ikavalenka.jd02_03;
+
+/**
+ * Created by USER on 10.04.2017.
+ */
+public interface ICashier {
+    void takeBuyerFromQueue();
+    void accountCashier();
+    void clientMayExit();
+    void calculateProfit();
+}
