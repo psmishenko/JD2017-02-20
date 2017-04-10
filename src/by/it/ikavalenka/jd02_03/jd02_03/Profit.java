741b36c0a9b0dfd7d0b264bc111ee74929fd007e
ikavalenka <z.yellow@yandex.by>
2017-04-11 01:08:35 +0300

    TASK fo REVIEW: jd02_03  in progress Note: Issue with Dispithcer class.

---
+++ src/by/it/ikavalenka/jd02_03/jd02_03/Profit.java
@@ -0,0 +1,34 @@
+package by.it.ikavalenka.jd02_03.jd02_03;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by USER on 11.04.2017.
+ */
+public class Profit {
+    private static Map<Integer,Integer> sumCashier = new ConcurrentHashMap<>();
+    private static int allProfit;
+    private final static Lock lockProfit = new ReentrantLock();
+
+    public static int getAllProfit(){
+        return allProfit;
+    }
+    public static Map<Integer,Integer> getSumCashier(){
+        return sumCashier;
+    }
+    public static void addCashierProfit(int cashierNumber,int profit){
+        int newProfit = sumCashier.containsKey(cashierNumber)?profit + sumCashier.get(cashierNumber):profit;
+        sumCashier.put(cashierNumber,newProfit);
+    }
+    public static void addSumProfit(int allProfit){
+        try{
+            lockProfit.lock();
+            allProfit=+allProfit;
+        } finally {
+            lockProfit.unlock();
+        }
+    }
+}
