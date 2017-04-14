5e0d93c1499332bad544bc10f43b9cb7b7c89d30
Prigozhanov <vprigozhanov@gmail.com>
2017-04-14 15:42:31 +0300

    my_matlab (in process) jd 02_07 ( in process)

--- src/by/it/prigozhanov/my_matlab/VarMatrix.java
+++ src/by/it/prigozhanov/my_matlab/VarMatrix.java
@@ -4,17 +4,25 @@ package by.it.prigozhanov.my_matlab;
  * Created by v-omf on 4/13/2017!
  */
 public class VarMatrix extends Var {
+    Double[][] matrix;
+
+
 
     public VarMatrix(String matrix) {
 
     }
 
-    public VarMatrix() {
+    public VarMatrix(VarMatrix var) {
     }
 
+    public VarMatrix(Double[][] matrix) {
+        
+    }
 
     @Override
     public void fromString(String stringValue) {
 
     }
+
+
 }
