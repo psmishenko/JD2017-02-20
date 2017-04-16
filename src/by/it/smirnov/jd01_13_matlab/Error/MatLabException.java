730e32aba18c600527cf693b907678e0d8c76a07
Александр Хмелев <Khmelov@users.noreply.github.com>
2017-04-16 12:32:44 +0300

    Update MatLabException.java

--- src/by/it/smirnov/jd01_13_matlab/Error/MatLabException.java
+++ src/by/it/smirnov/jd01_13_matlab/Error/MatLabException.java
@@ -1,7 +1,7 @@
 package by.it.smirnov.jd01_13_matlab.Error;
 
 /**
- * Класс ошибки (Exception), связанной с MatLabClassWork
+ * Класс ошибки (Exception), связанной с MatLab
  */
 public class MatLabException extends Exception{
     public MatLabException(String message) {
