6ad60aa82ed4982e9dff082815fd0ac14681a2f5
ikavalenka <z.yellow@yandex.by>
2017-04-17 00:27:22 +0300

    TASK fo REVIEW: jd02_06 TaskA, Math_lab in progress...

---
+++ src/by/it/ikavalenka/jd02_06/Client.java
@@ -0,0 +1,17 @@
+package by.it.ikavalenka.jd02_06;
+
+/**
+ * Created by USER on 12.04.2017.
+ */
+public class Client {
+    public static void main(String[] args) {
+        Logger log1 = Logger.getInstance();
+        log1.log("one");
+        log1.log("two");
+
+//        Logger log2 =Logger.getInstance();
+//        log.log("one");
+//        log.log("two");
+    }
+
+}
