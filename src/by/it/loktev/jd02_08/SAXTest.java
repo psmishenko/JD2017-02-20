726c48778bd72db216c8385595314a354f387d30
loktevalexey <loktev.alex.74@gmail.com>
2017-04-17 10:32:12 +0300

    jd02_08

---
+++ src/by/it/loktev/jd02_08/SAXTest.java
@@ -0,0 +1,32 @@
+package by.it.loktev.jd02_08;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class SAXTest {
+
+    private static String XMLFileName=System.getProperty("user.dir")+"/src/by/it/loktev/jd02_08/picture.xml";
+
+    public static void main(String[] args) {
+
+        try {
+            SAXParserFactory spf=SAXParserFactory.newInstance();
+            SAXParser sp=spf.newSAXParser();
+            SAXHandler sh=new SAXHandler();
+            sp.parse(new File(XMLFileName),sh);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
