bfbb7d03d12d932dfc88ed497dff0c59d784839f
anteresh <anteresh@gmail..com>
2017-04-18 00:27:56 +0200

    jd02_08 added

---
+++ src/by/it/tereshko/jd02_08/StAXRunner.java
@@ -0,0 +1,31 @@
+package by.it.tereshko.jd02_08;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class StAXRunner {
+
+    private static String filenameXML = "src/by/it/tereshko/jd02_08/Persons.xml";
+
+    public static void main(String[] args) {
+
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+
+        try (FileInputStream fileInputStream = new FileInputStream(filenameXML)) {
+            XMLStreamReader reader = inputFactory.createXMLStreamReader(fileInputStream);
+
+            StAX instance = new StAX(reader);
+            String txt = instance.parse();
+            System.out.println(txt);
+
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
