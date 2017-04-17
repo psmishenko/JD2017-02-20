492559ef5bb4ff5ee68e03d078eab473e9c6ce33
VladVedom <house9494@mail.ru>
2017-04-17 18:59:59 +0300

    jd02_08

---
+++ src/by/it/vedom/jd02_08/SAXRuner.java
@@ -0,0 +1,32 @@
+package by.it.vedom.jd02_08;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
+
+public class SAXRuner {
+
+    public static String filenameXML = "src/by/it/vedom/jd02_08/Test.xml";
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException {
+
+        try {
+            SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            SAX handler = new SAX();
+            saxParser.parse(new File(filenameXML), handler);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
