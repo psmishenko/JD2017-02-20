ac59ed97ec229067511efdd5f379c778331c8814
Paul <psmishenko@gmail.com>
2017-04-18 20:08:28 +0300

    jd02_8 (18.04)

--- src/by/it/psmishenko/jd02_8/DOM.java
+++ src/by/it/psmishenko/jd02_8/DOM.java
@@ -8,11 +8,19 @@ import org.w3c.dom.NodeList;
  * Created by GN on 17.04.2017.
  */
 public class DOM {
-    StringBuilder text ;
+   public static StringBuilder text = new StringBuilder() ;
+   private String tab = "\n";
 
-     void parseDOM(Node node){
+     void parseDOM(Node node, String pr){
+         String txt = node.getNodeValue();
+        if(txt!=null) {
+            text.append(pr).append(txt.trim());
+        }
+        NodeList ch = node.getChildNodes();
+         for (int i = 0; i <ch.getLength() ; i++) {
+             parseDOM(ch.item(i),tab+"<"+node.getNodeName()+">");
 
+         }
     }
-
 
 }
