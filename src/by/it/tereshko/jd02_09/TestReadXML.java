fc24023f88c5f1f2a100fd14df136f41469cadd4
anteresh <anteresh@gmail.com>
2017-04-19 12:33:20 +0300

    jd02_09 added

---
+++ src/by/it/tereshko/jd02_09/TestReadXML.java
@@ -0,0 +1,38 @@
+package by.it.tereshko.jd02_09;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class TestReadXML {
+    public static void main(String[] args) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Persons.class);
+            Unmarshaller u = context.createUnmarshaller();
+            String path = "src/by/it/tereshko/jd02_09/Persons.xml";
+            FileReader fileReader = new FileReader(new File(path));
+            Object o = u.unmarshal(fileReader);
+            Persons persons = (Persons) o;
+            System.out.println(persons);
+
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(persons, System.out);
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            System.out.println(gson.toJson(persons));
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/by/it/tereshko/jd02_09/generate.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk1.7.0_80\bin\xjc.exe" D:\students\JD2017-02-20\src\by\it\tereshko\jd02_09\Person.xsd -d D:\students\JD2017-02-20\src
