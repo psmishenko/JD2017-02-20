86c6284760e0b795d5cb8ea017d932c399400e90
loktevalexey <loktev.alex.74@gmail.com>
2017-04-19 11:35:43 +0300

    jd02_09

---
+++ src/by/it/loktev/jd02_09/xjc.bat
@@ -0,0 +1,22 @@
+@echo off
+@echo При неободимости вам нужно подставить тут свои пути
+
+@echo исправьте три пути 1) к JDK 2) к src 3) к xsd
+@echo обратите внимание, что слеши в конце не нужны
+@echo ну и конечно, на linux будет совершенно другой синтаксис
+
+set path_xjc="D:\Java\jdk1.8.0_121\bin\xjc.exe"
+set path_to_src="..\..\..\..\..\src"
+set file_xcd="%path_to_src%\by\it\loktev\jd02_09\picture.xsd"
+
+:: Пути могут быть и абсолютными, вроде таких
+:: set path_xjc="H:\Program Files\Java\jdk1.8.0_101\bin\xjc.exe"
+:: set path_to_src="C:\JavaProjects\JD2017-02-20\src"
+:: set file_xcd="C:\JavaProjects\JD2017-02-20\src\by\it\akhmelev\jd02_09\generate\xml_schema.xsd"
+:: :: - это комментарий
+
+@echo формируем команду (все три переменные - в одну строку)
+@echo Команда будет такой: %path_xjc% %file_xcd% -d %path_to_src%
+
+@echo on
+%path_xjc% %file_xcd% -d %path_to_src%
