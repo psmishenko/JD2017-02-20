c0e69405ec5cf3808bbf1b0c0a5dae4798a5b2f3
loktevalexey <loktev.alex.74@gmail.com>
2017-04-19 12:22:36 +0300

    jd02_09

--- src/by/it/loktev/jd02_08/Runner.java
+++ src/by/it/loktev/jd02_08/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        String XMLFileName=System.getProperty("user.dir")+"/src/by/it/loktev/jd02_08/picture.xml";
+        String XMLFileName=System.getProperty("user.dir")+"/src/by/it/loktev/jd02_08/group.xml";
 
         SAXTest.run(XMLFileName);
 
