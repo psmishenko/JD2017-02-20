aef95cee6139ecbfbf9c1678952e866609c8e61a
loktevalexey <loktev.alex.74@gmail.com>
2017-04-20 13:21:15 +0300

    jd02_09/abc, jd02_10/a

--- src/by/it/loktev/jd02_10/TaskA.java
+++ src/by/it/loktev/jd02_10/TaskA.java
@@ -7,25 +7,37 @@ public class TaskA {
 
     public static void run() {
 
-        Square square=new Square();
-        square.setLeft(100);
-        square.setTop(50);
-        square.setWidth(80);
-        square.setHeight(30);
-        square.setFill("yellow");
-        square.setStroke("blue");
-        System.out.println("=== тестовый POJO-объект:");
-        System.out.println(square.toString());
+        Picture p=new Picture();
 
-        /*
-        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
-        //маршаллизация
-        String json = gson.toJson(dev);
-        System.out.print(json);
-        //демаршаллизация
-        Dev dev2=gson.fromJson(json, Dev.class);
-        System.out.print("\n\ndev2=" + dev2.toString());
-        */
+        Square s1=new Square();
+        s1.setLeft(100);
+        s1.setTop(50);
+        s1.setWidth(80);
+        s1.setHeight(30);
+        s1.setFill("yellow");
+        s1.setStroke("blue");
+
+        Circle c1=new Circle();
+        c1.setCenterx(200);
+        c1.setCentery(250);
+        c1.setRadius(110);
+        c1.setFill("white");
+        c1.setStroke("black");
+
+        p.squareOrCircleOrText.add(s1);
+        p.squareOrCircleOrText.add(c1);
+
+        System.out.println("=== тестовый POJO-объект:");
+        System.out.println(p.toString());
+
+        Gson gson=new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        String pictureJSON=gson.toJson(p);
+        System.out.println("=== JSON-представление:");
+        System.out.println(pictureJSON);
+
+        Picture p2=gson.fromJson(pictureJSON, Picture.class);
+        System.out.println("=== восстановленный из JSON POJO-объект:");
+        System.out.println(p2.toString());
     }
 }
 
