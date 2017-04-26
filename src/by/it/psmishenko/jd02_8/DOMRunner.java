d60330165cfaf056694fa8e829901b8d4af9b641
Paul <psmishenko@gmail.com>
2017-04-26 18:31:56 +0300

    jd02_9-10 final

--- src/by/it/psmishenko/jd02_8/DOMRunner.java
+++ src/by/it/psmishenko/jd02_8/DOMRunner.java
@@ -15,18 +15,23 @@ import java.io.IOException;
  * Created by GN on 17.04.2017.
  */
 public class DOMRunner {
-    private static String fileNameXML = System.getProperty("user.dir")+"\\src\\by\\it\\psmishenko\\jd02_8\\Test2.xml";
-
+    private static String fileNameXML1 = System.getProperty("user.dir")+"\\src\\by\\it\\psmishenko\\jd02_8\\Test.xml"; // без атрибутов (моя предметная область)
+    private static String fileNameXML2 = System.getProperty("user.dir")+"\\src\\by\\it\\psmishenko\\jd02_8\\Test2.xml"; // с атрибутами (произвольный xml файл)
     public static void main(String[] args) {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
+
             DocumentBuilder builder = factory.newDocumentBuilder();
-                Document document = builder.parse(new File(fileNameXML));
+                Document document = builder.parse(new File(fileNameXML1));
             Element element = document.getDocumentElement();
             DOM dom = new DOM();
             dom.parseDOM(element,"");
-            System.out.println(DOM.text);
-
+            System.out.println("XML по моей предметной области (без атрибутов)\n"+dom.getText());
+            Document document1 = builder.parse(new File(fileNameXML2));
+            Element element1 = document1.getDocumentElement();
+            DOM dom1 = new DOM();
+            dom1.parseDOM(element1,"");
+            System.out.println("Произвольный XML с атрибутами\n"+dom1.getText());
             }catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (SAXException e) {
