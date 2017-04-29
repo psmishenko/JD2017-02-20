b6e83f814f254f1830e08a3f6955836d8cc87f92
user <psmishenko@gmail>
2017-04-29 13:23:13 +0300

    cw 29.04

---
+++ src/by/it/psmishenko/jd03_2/test_crud/TestUser.java
@@ -0,0 +1,24 @@
+package by.it.psmishenko.jd03_2.test_crud;
+
+import by.it.psmishenko.jd03_2.beans.User;
+import by.it.psmishenko.jd03_2.crud.UserCrud;
+
+import java.sql.SQLException;
+
+/**
+ * Created by user on 29.04.2017.
+ */
+public class TestUser {
+    public static void main(String[] args) throws SQLException {
+        UserCrud userCrud = new UserCrud();
+        User user = new User(0,"LTest","PTest","ETest",2);
+        System.out.println(user);
+        if(userCrud.create(user)) System.out.println("Create:"+user.getLogin());
+        user = userCrud.read(user);
+        if(user!=null) System.out.println("read:"+user.getLogin());
+        user.setEmail("vdfvdfvdvdf");
+        if(userCrud.update(user)) System.out.println("update:"+user.getLogin());
+        if(userCrud.delete(user)) System.out.println("delete");
+
+    }
+}
