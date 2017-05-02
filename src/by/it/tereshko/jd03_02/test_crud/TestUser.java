1811d4dc3e05703788c7bdc0c191b9fbdb26cdda
anteresh <anteresh@gmail..com>
2017-05-03 00:06:00 +0200

    jd03_01 & jd03_02 added

---
+++ src/by/it/tereshko/jd03_02/test_crud/TestUser.java
@@ -0,0 +1,27 @@
+package by.it.tereshko.jd03_02.test_crud;
+
+import by.it.tereshko.jd03_02.beans.User;
+import by.it.tereshko.jd03_02.crud.UserCRUD;
+
+import java.sql.SQLException;
+
+public class TestUser {
+
+    public static void main(String[] args) throws SQLException {
+
+        UserCRUD userCRUD = new UserCRUD();
+        User user = new User(0, "Ltest", "Ptest", "Etest", 2);
+        System.out.println(user);
+        if (userCRUD.create(user))
+            System.out.println("CREATE: " + user);
+        user = userCRUD.read(user);
+        if (user != null)
+            System.out.println("READ: " + user);
+        user.setEmail("djhfgdkjsgfdasg");
+        if (userCRUD.update(user))
+            System.out.println("UPDATE: " + user);
+        if (userCRUD.delete(user))
+            System.out.println("DELETE: " + user);
+
+    }
+}
