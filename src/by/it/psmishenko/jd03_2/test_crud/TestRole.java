3ded1e05ae812ef1fea716709869f9ea997ba01e
Paul <psmishenko@gmail.com>
2017-05-02 18:45:54 +0300

    jd03_2 (02.05)

--- src/by/it/psmishenko/jd03_2/test_crud/TestRole.java
+++ src/by/it/psmishenko/jd03_2/test_crud/TestRole.java
@@ -3,13 +3,22 @@ package by.it.psmishenko.jd03_2.test_crud;
 import by.it.psmishenko.jd03_2.beans.Role;
 import by.it.psmishenko.jd03_2.crud.RoleCrud;
 
+import java.sql.SQLException;
+
 /**
  * Created by user on 29.04.2017.
  */
 public class TestRole {
-    public static void main(String[] args) {
+    public static void run() throws SQLException {
         RoleCrud roleCrud = new RoleCrud();
-        Role role = new Role(0,"Admin");
-        roleCrud.create(role);
+        Role role = new Role(0,"Admin123456");
+        if(roleCrud.create(role)) System.out.println("Create new role:"+role.getRole()+" ID = "+role.getId());
+        role = roleCrud.read(role);
+        if(role!=null) System.out.println("read role:"+role.getRole()+" ID = "+role.getId());
+        role.setRole("Admin789");
+        if(roleCrud.update(role)) System.out.println("update:"+role.getRole());
+        if(roleCrud.delete(role)) System.out.println("delete role");
+        System.out.println("User ID = "+roleCrud.getRoleID("User"));
+        System.out.println("Admin ID = "+roleCrud.getRoleID("Admin"));
     }
 }
