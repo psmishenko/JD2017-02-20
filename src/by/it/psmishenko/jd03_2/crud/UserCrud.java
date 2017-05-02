0d4c78f3b0105d7d6cdbabb91bdf96dc1f35e298
Paul <psmishenko@gmail.com>
2017-05-02 18:45:54 +0300

    jd03_2 (02.05)

--- src/by/it/psmishenko/jd03_2/crud/UserCrud.java
+++ src/by/it/psmishenko/jd03_2/crud/UserCrud.java
@@ -27,24 +27,43 @@ public class UserCrud {
         }
         return false;
     }
-    public  boolean update(User user){
-        return false;
+    public  boolean update(User user) throws SQLException {
+        try (Connection connection = ConnectorCreator.getConnection();
+             Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "UPDATE `users` SET `Login`='%s',`Password`='%s',`Email`='%s'," +
+                            "`FK_roles`=%d WHERE ID=%d",
+                    user.getLogin(),
+                    user.getPassword(),
+                    user.getEmail(),
+                    user.getFk_roles(),
+                    user.getId()
+            );
+            return (1 == statement.executeUpdate(sql));
+        }
     }
     public  User read(User user) throws SQLException {
         User resultUser = null;
         try(Connection connection = ConnectorCreator.getConnection();
             Statement statement = connection.createStatement()) {
-            String sql = String.format(" SELECT `users`(`Login`, `Password`, `Email`, `FK_Roles`) VALUES ('%s','%s','%s','%d')"    // SELECT!!!!!!!!!!!!!!!!!
-                    , user.getLogin(), user.getPassword(), user.getEmail(), user.getFk_roles());
+            String sql = String.format("SELECT `ID`, `Login`, `Password`, `Email`, `FK_roles` FROM `users` WHERE ID = %d "
+                    ,user.getId());
             ResultSet rs = statement.executeQuery(sql);
             if(rs.next()){
-              //  resultUser = new User(rs.getInt("ID"),rs.getString("Login"),rs.getString("Password")) // new user
-
+                resultUser = new User(rs.getInt("ID"),rs.getString("Login")
+                        ,rs.getString("Password"),rs.getString("Email"),rs.getInt("FK_roles"));
             }
         }
         return resultUser;
     }
-    public  boolean delete(User user){
-        return false;
+    public  boolean delete(User user) throws SQLException {
+        try (Connection connection = ConnectorCreator.getConnection();
+             Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "DELETE FROM `users` WHERE ID=%d",
+                    user.getId()
+            );
+            return (1 == statement.executeUpdate(sql));
+        }
     }
 }
