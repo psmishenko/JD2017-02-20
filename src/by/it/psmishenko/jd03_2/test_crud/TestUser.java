c917477fe041cbf8e343f02888862b5b22fb293f
Paul <psmishenko@gmail.com>
2017-05-02 18:45:54 +0300

    jd03_2 (02.05)

--- src/by/it/psmishenko/jd03_2/test_crud/TestUser.java
+++ src/by/it/psmishenko/jd03_2/test_crud/TestUser.java
@@ -9,15 +9,15 @@ import java.sql.SQLException;
  * Created by user on 29.04.2017.
  */
 public class TestUser {
-    public static void main(String[] args) throws SQLException {
+    public static void run() throws SQLException {
         UserCrud userCrud = new UserCrud();
-        User user = new User(0,"LTest","PTest","ETest",2);
+        User user = new User(0,"LTest","PTest","ETest",47);
         System.out.println(user);
         if(userCrud.create(user)) System.out.println("Create:"+user.getLogin());
         user = userCrud.read(user);
         if(user!=null) System.out.println("read:"+user.getLogin());
         user.setEmail("vdfvdfvdvdf");
-        if(userCrud.update(user)) System.out.println("update:"+user.getLogin());
+        if(userCrud.update(user)) System.out.println("update:"+user.getLogin()+" Email:"+user.getEmail());
         if(userCrud.delete(user)) System.out.println("delete");
 
     }
