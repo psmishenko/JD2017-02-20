9bd5154f58bb1d0712b3ea7c4a8fe59a66eb92c2
loktevalexey <loktev.alex.74@gmail.com>
2017-05-03 15:44:25 +0300

    jd03_03/a

--- src/by/it/loktev/jd03_03/Runner.java
+++ src/by/it/loktev/jd03_03/Runner.java
@@ -1,6 +1,12 @@
 package by.it.loktev.jd03_03;
 
+import by.it.loktev.jd03_03.DAO.RoleDAO;
+import by.it.loktev.jd03_03.DAO.UserDAO;
+import by.it.loktev.jd03_03.beans.Role;
+import by.it.loktev.jd03_03.beans.User;
+
 import java.sql.SQLException;
+import java.util.List;
 
 public class Runner {
 
@@ -14,6 +20,22 @@ public class Runner {
             TestRole.run();
 
             TestUser.run();
+
+            UserDAO userDAO=new UserDAO();
+            RoleDAO roleDAO=new RoleDAO();
+            List<User> users=userDAO.getAll("");
+            for ( User user : users ){
+                int roleid=user.getRoleId();
+                Role role=roleDAO.read(roleid);
+                System.out.println("Пользователь " +
+                        "id=" + user.getId() +
+                        " login=" + user.getLogin() +
+                        " password=" + user.getPassword() +
+                        " email=" + user.getEmail() +
+                        " role=" + role.getName()
+                        );
+
+            }
 
             //System.out.println("\n=== Вариант B\n");
 
