4e60f839720e543eb8ea58853ff986438becc426
Alex <akhmelev@gmail.com>
2017-05-05 00:13:22 +0300

    jd03_04 demo

---
+++ src/by/it/akhmelev/jd03_04/java/sample_controller/FrontController.java
@@ -0,0 +1,21 @@
+package by.it.akhmelev.jd03_04.java.sample_controller;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter prn=resp.getWriter();
+        prn.println("<br><br>DEMO: It is Sample Front Controller!");
+        prn.println("<br><br>Please create Your Front Controller!");
+        prn.println("<br><br>URI:"+req.getRequestURI());
+        prn.flush();
+    }
+
+}
