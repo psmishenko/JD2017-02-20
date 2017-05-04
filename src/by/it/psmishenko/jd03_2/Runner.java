6ae669c648b146cc97f07392ab18bf4f9c4d214b
Paul <psmishenko@gmail.com>
2017-05-04 19:13:57 +0300

    jd03_2 , jd03_3  (04.05) add

--- src/by/it/psmishenko/jd03_2/Runner.java
+++ src/by/it/psmishenko/jd03_2/Runner.java
@@ -1,5 +1,7 @@
 package by.it.psmishenko.jd03_2;
 
+import by.it.psmishenko.jd03_2.crud.Init;
+import by.it.psmishenko.jd03_2.crud.ResetTables;
 import by.it.psmishenko.jd03_2.crud.ShowUsers;
 import by.it.psmishenko.jd03_2.test_crud.TestBook;
 import by.it.psmishenko.jd03_2.test_crud.TestRole;
@@ -12,6 +14,10 @@ import java.sql.SQLException;
  */
 public class Runner {
     public static void main(String[] args) throws SQLException {
+        ResetTables.resetAllDB(); //Удаления всех таблиц в вашейбазе данных
+        Init.recoveryDB(); // Создания всех таблиц
+        //Заполнения таблиц через CRUD операции (прописать заполнение напрямую вызовом конструкторов объектов (beans) с нужными параметрами)
+        Init.enterData();
         TestRole.run();
         TestUser.run();
         TestBook.run();
