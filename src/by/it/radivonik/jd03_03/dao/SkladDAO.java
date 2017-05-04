289c99af85b72990497050dd2e79b140fbb32f27
RadivonikAlexander <olsy@tut.by>
2017-05-05 00:31:09 +0300

    jd03_01, jd03_02, jd03_03

---
+++ src/by/it/radivonik/jd03_03/dao/SkladDAO.java
@@ -0,0 +1,49 @@
+package by.it.radivonik.jd03_03.dao;
+
+import by.it.radivonik.jd03_03.beans.Sklad;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Radivonik on 05.05.2017.
+ */
+public class SkladDAO extends AbstractDAO<Sklad> implements InterfaceDAO<Sklad>  {
+    @Override
+    protected String sqlSelect() {
+        return "SELECT * FROM sklad";
+    }
+
+    @Override
+    protected String sqlInsert(Sklad sklad) {
+        String sql = "INSERT INTO sklad (count, cena, id_tovar, id_user) VALUES (%s, %s, %d, %d)";
+        return String.format(sql, sklad.getCount(), sklad.getCena(), sklad.getIdTovar(), sklad.getIdUser());
+    }
+
+    @Override
+    protected String sqlUpdate(Sklad sklad) {
+        String sql = "UPDATE sklad SETcount = %s, cena = %s, id_tovar = %d, id_user = %d  WHERE id = %d";
+        return String.format(sql, sklad.getCount(), sklad.getCena(), sklad.getIdTovar(), sklad.getIdUser(), sklad.getId());
+    }
+
+    @Override
+    protected String sqlDelete(Sklad sklad) {
+        String sql = "DELETE FROM sklad WHERE id = %d";
+        return String.format(sql, sklad.getId());
+    }
+
+    @Override
+    protected void setId(Sklad sklad, int id) {
+        sklad.setId(id);
+    }
+
+    @Override
+    protected Sklad newBean(ResultSet resultSet) throws SQLException {
+        Sklad sklad = new Sklad(
+            resultSet.getInt("id"),
+            resultSet.getBigDecimal("count"),
+            resultSet.getBigDecimal("cena"),
+            resultSet.getInt("id_tovar"),
+            resultSet.getInt("id_user"));
+        return sklad;
+    }
+}
