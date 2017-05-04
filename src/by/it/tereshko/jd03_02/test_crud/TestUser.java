5fa78766055fb08ae35a1afdc2a32be21d91cc02
anteresh <anteresh@gmail..com>
2017-05-05 00:27:36 +0200

    jd03_03 added

--- src/by/it/tereshko/jd03_02/test_crud/TestUser.java
+++ src/by/it/tereshko/jd03_02/test_crud/TestUser.java
@@ -10,14 +10,14 @@ public class TestUser {
     public static void main(String[] args) throws SQLException {
 
         UserCRUD userCRUD = new UserCRUD();
-        User user = new User(0, "Ltest", "Ptest", "Etest", 2);
+        User user = new User(0, "MyTestLogin", "MyTestPassword", "MyTestEmail", 2);
         System.out.println(user);
         if (userCRUD.create(user))
             System.out.println("CREATE: " + user);
         user = userCRUD.read(user);
         if (user != null)
             System.out.println("READ: " + user);
-        user.setEmail("djhfgdkjsgfdasg");
+        user.setEmail("thisismyemail@wow.com");
         if (userCRUD.update(user))
             System.out.println("UPDATE: " + user);
         if (userCRUD.delete(user))
---
+++ src/by/it/tereshko/jd03_03/dao/CN.java
@@ -0,0 +1,9 @@
+package by.it.tereshko.jd03_03.dao;
+
+interface CN {
+
+    String URL_DB = "jdbc:mysql://127.0.0.1:2016/tereshko" + "?useUnicode=true&characterEncoding=UTF-8";
+    String USER_DB = "root";
+    String PASSWORD_DB = "";
+
+}
