38427d2c1d938b934c8606ecf1d4e89ac0b261d5
zzz <zz88@tut.by>
2017-05-05 12:44:24 +0300

    project

--- src/by/it/kolenda/project/java/controller/CommandError.java
+++ src/by/it/kolenda/project/java/controller/CommandError.java
@@ -1,7 +1,13 @@
 package by.it.kolenda.project.java.controller;
 
+import com.sun.deploy.net.HttpRequest;
+
 /**
  * Created by user on 05.05.2017.
  */
-public class CommandError {
+public class CommandError implements ActionComand {
+    @Override
+    public String execute(HttpRequest request) {
+        return "/index.jsp";
+    }
 }
