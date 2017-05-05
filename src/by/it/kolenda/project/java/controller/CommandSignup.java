f6a77f7d46f9fa5e2d2ee31937937a1dc84e0955
zzz <zz88@tut.by>
2017-05-05 12:42:19 +0300

    project

--- src/by/it/kolenda/project/java/controller/CommandSignup.java
+++ src/by/it/kolenda/project/java/controller/CommandSignup.java
@@ -1,15 +1,7 @@
 package by.it.kolenda.project.java.controller;
 
-import com.sun.deploy.net.HttpRequest;
-
 /**
  * Created by user on 05.05.2017.
  */
-public class CommandSignup implements ActionComand{
-
-    @Override
-    public String execute(HttpRequest request) {
-        return "/index.jsp";
-    }
+public class CommandSignup {
 }
-
