763b613fcf1638c2c8a333091853fe11b5635cdd
RadivonikAlexander <olsy@tut.by>
2017-05-07 12:32:00 +0300

    jd03_01, jd03_02

--- src/by/it/radivonik/jd03_02/TaskA.java
+++ src/by/it/radivonik/jd03_02/TaskA.java
@@ -1,7 +1,9 @@
 package by.it.radivonik.jd03_02;
 
 import by.it.radivonik.jd03_02.beans.Role;
+import by.it.radivonik.jd03_02.beans.User;
 import by.it.radivonik.jd03_02.crud.RoleCRUD;
+import by.it.radivonik.jd03_02.crud.UserCRUD;
 
 import java.sql.SQLException;
 
@@ -18,5 +20,25 @@ public class TaskA {
 
         else
             System.out.printf("Роль: '%s', id: %d\n", role.getName(), role.getId());
+
+        System.out.printf("Создание нового пользователя:\n");
+        User user = new User(0, "new_login", "new_password", "new_email", 2);
+        UserCRUD userCRUD = new UserCRUD();
+        userCRUD.create(user);
+        user = userCRUD.read(user);
+        System.out.println(user);
+
+        System.out.printf("Изменение пользователя:\n");
+        user.setLogin("new_login_2");
+        user.setPassword("new_password_2");
+        user.setEmail("new_email_2");
+        userCRUD.update(user);
+        user = userCRUD.read(user);
+        System.out.println(user);
+
+        System.out.printf("Удаление пользователя:\n");
+        userCRUD.delete(user);
+        user = userCRUD.read(user);
+        System.out.println(user);
     }
 }
