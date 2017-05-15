805295121a6ca592a0f561248c04c7686f4f2224
Khmelov <375336849110@tut.by>
2017-05-15 11:04:50 +0300

    jd03_08 pull of connections

--- src/by/it/akhmelev/project/java/connection/ConnectionCreator.java
+++ src/by/it/akhmelev/project/java/connection/ConnectionCreator.java
@@ -1,18 +1,13 @@
 package by.it.akhmelev.project.java.connection;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionCreator {
-    //Памятка. Корректно держать настройки соединения вне кода (!)
-    private static final String URL_DB =
-            "jdbc:mysql://127.0.0.1:2016/it_academy"
-                    + "?useUnicode=true&characterEncoding=UTF-8";
-    private static final String USER_DB = "root";
-    private static final String PASSWORD_DB = "";
-    private static volatile Connection connection = null;
-
     static {
         try { //регистрируем драйвер
             Class.forName("com.mysql.jdbc.Driver");
@@ -22,10 +17,38 @@ public class ConnectionCreator {
     }
 
     public static Connection getConnection() throws SQLException {
+        try {
+            InitialContext ic=new InitialContext();
+            DataSource ds=(DataSource) ic.lookup("java:/comp/env/jdbc/my_sql_akhmelev");
+            return ds.getConnection();
+        } catch (NamingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+        //Памятка. Корректно держать настройки соединения вне кода (!)
+    /*
+    private static final String URL_DB =
+            "jdbc:mysql://127.0.0.1:2016/it_academy"
+                    + "?useUnicode=true&characterEncoding=UTF-8";
+    private static final String USER_DB = "root";
+    private static final String PASSWORD_DB = "";
+    private static volatile Connection connection = null;
+
+
+    public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed())
             connection = DriverManager.getConnection(
                     URL_DB, USER_DB, PASSWORD_DB
             );
         return connection;
     }
+
+    */
+
+
+
+
 }
